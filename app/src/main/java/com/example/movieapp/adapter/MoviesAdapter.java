038491ccc9185b5375fa2ffa6fd9679c913aceed
johncarloagetano <johncarloagetano-at-363923524953>
2023-06-14 11:33:26 +0800

    upload project

---
+++ app/src/main/java/com/example/movieapp/adapter/MoviesAdapter.java
@@ -0,0 +1,37 @@
+package com.example.movieapp.adapter;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.movieapp.databinding.ItemMovieBinding;
+
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewHolder> {
+
+    @NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class MovieViewHolder extends RecyclerView.ViewHolder {
+        private ItemMovieBinding itemMovieBinding;
+
+        public MovieViewHolder(@NonNull ItemMovieBinding itemMovieBinding) {
+            super(itemMovieBinding.getRoot());
+            this.itemMovieBinding = itemMovieBinding;
+
+        }
+    }
+}
